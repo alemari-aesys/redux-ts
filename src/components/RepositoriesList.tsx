a771b0dd630e8adda26328146263e59b392b971f
Alessandro Marinelli <alessandro.marinelli@aesys.tech>
2021-07-15 15:27:01 +0200

    first commit

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
+
+const RepositoriesList: React.FC = () => {
+
+    const [term, setTerm] = useState("");
+    const { searchRepositories } = useActions();
+    const { data, error, loading } = useTypedSelector((state) => state.repositories)
+    
+
+    const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        searchRepositories(term)
+    }
+
+    return <div>
+        <form onSubmit={onSubmit}>
+            <input type="text" value={term} onChange={e => setTerm(e.target.value)} />
+            <button>Search</button>
+        </form>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>loading...</h3>}
+        {!error && !loading && data.map((x, i) => <div key={i}>{x}</div>)}
+    </div>
+}
+
+export default RepositoriesList;
